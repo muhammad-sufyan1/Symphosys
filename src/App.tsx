3f53e20a23f6eaaba05c1c667e8adf1ce2030cdc
Abdul Waheed <usman.anwar@physik.uni-kl.de>
2026-03-06 04:31:46 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { CookieBanner } from './components/CookieBanner';
